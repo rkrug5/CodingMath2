22b3b92a974779a960406709b53c7d896d7398b0
rkrug5 <phishermankrug@hotmail.com>
2018-04-08 19:08:47 -0700

    fixed bug in ep3

---
+++ ep3/main.js
@@ -0,0 +1,44 @@
+
+
+//following along with code from:
+// https://www.youtube.com/user/codingmath
+//it is a great resource, you should check it out!
+
+
+
+
+window.onload = function () {
+
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext('2d'),
+		width = canvas.width = window.innerWidth;
+	height = canvas.height = window.innerHeight;
+
+
+	var centerY = height * .5,
+		centerX = width * .5,
+		offset = height * .4,
+		speed = 0.1,
+		angle = 0;
+
+
+	render();
+
+	function render() {
+		var y = centerY + Math.sin(angle) * offset;
+
+		context.clearRect(0, 0, width, height);
+		context.beginPath();
+		context.arc(centerX, y, 50, 0, Math.PI * 2, false);
+		context.fill();
+
+
+		angle += speed;
+
+		requestAnimationFrame(render);
+	}
+
+
+
+
+};
